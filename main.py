5af112567b56a190097e3f4da1d621f6aae88943
PY-TU-01-24 <marek.senk@gmail.com>
2024-04-02 20:09:46 +0200

    new class 'Student', new function 'sayHi'

--- main.py
+++ main.py
@@ -1,12 +1,33 @@
-veta = 'Na železnici za Čelákovicemi ve směru na Prahu došlo k poruše trakčního vedení, potvrdil pro CNN Prima NEWS tiskový mluvčí Českých drah Filip Medelský. Incident je vyšetřován jako mimořádná událost.'
-#1
-mySet = set()
-for _ in veta:
-  mySet.add(_)
-print(mySet)
-#mySet = set(veta)
-#2
-for _ in mySet:
-  print(_  + " " + str(veta.count(_)))
-print('něco na tom bude')
-print('123456')
+class Student:
+
+  spec = 'devOps'  # spolecna vlastnost pro vsechny objekty
+  def __init__(self, name, age):
+    self.name = name
+    self.age = age
+
+  def show_msg(self):
+    print(f'\nMoje jméno je {self.name} a můj věk je {self.age}')
+
+  def say_hi(self, pozdrav):
+    print(f'\n{pozdrav}, já jsem {self.name}')
+
+
+student1 = Student('Adam', 55)
+student1.show_msg()
+
+student2 = Student('Osel', 99)
+student1.say_hi('Ahoj')
+student2.say_hi('Dobrý den')
+
+# student1 = Student('Pavel', 2000)
+#
+# print(student1.name)
+#
+# student2 = Student('Adam', 1999)
+# print(student2.age)
+# print(student2.name)
+# print(student2.spec)
+#
+# student2.name = 'Petr'
+# print(f'Nové jméno je {student2.name}')
+
